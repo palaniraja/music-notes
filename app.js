9057fc511c06d65598e20b333746632facb88960
palaniraja <palaniraja@live.com>
2017-03-11 22:58:23 +0530

    initial commit to github

---
+++ app.js
@@ -0,0 +1,29 @@
+
+import React, { Component } from 'react';
+
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+
+
+
+export default class App extends Component {
+  
+  render() {
+    return (
+      <View>
+      <Text></Text>
+      <Text>
+      Let us learn to read Music notes
+      </Text>
+      </View>
+    );
+  }
+}
+
+
+
---
+++ index.android.js
@@ -0,0 +1,10 @@
+
+import {
+  AppRegistry,
+} from 'react-native';
+
+
+import App from './app'
+
+
+AppRegistry.registerComponent('mnotes', () => App);
