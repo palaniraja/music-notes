dc1b61b655969ff73c7e3267e86a5e61f4cdc54d
palaniraja <palaniraja@live.com>
2017-03-14 23:42:40 +0530

    initial attempt for levelbased flashcard

--- src/flashcard.js
+++ src/flashcard.js
@@ -10,10 +10,12 @@ import {
   TouchableHighlight,
   Button,
   Image,
+  Picker,
+  Slider,
 } from 'react-native';
 
 
-
+const cheatImg = {name: "", image: require('./assets/cheat.png')};
 
 export default class FlashCard extends Component {
 
@@ -25,29 +27,13 @@ export default class FlashCard extends Component {
 
   constructor(props) {
     super(props);
-    const ds = [
-      { name: '', image: require('./assets/cheat.png')}, 
-      { name: 'c4', image: require('./assets/c4.png')}, 
-      { name: 'd4', image: require('./assets/d4.png')}, 
-      { name: 'e4', image: require('./assets/e4.png')}, 
-      { name: 'f4', image: require('./assets/f4.png')}, 
-      { name: 'g4', image: require('./assets/g4.png')}, 
-      { name: 'a4', image: require('./assets/a4.png')}, 
-      { name: 'b4', image: require('./assets/b4.png')},
-      { name: 'c5', image: require('./assets/c5.png')}, 
-      { name: 'd5', image: require('./assets/d5.png')}, 
-      { name: 'e5', image: require('./assets/e5.png')}, 
-      { name: 'f5', image: require('./assets/f5.png')}, 
-      { name: 'g5', image: require('./assets/g5.png')}, 
-      { name: 'a5', image: require('./assets/a5.png')}, 
-      { name: 'b5', image: require('./assets/b5.png')}
-    ];
     this.state = {
-      noteImages: ds,
-      length: ds.length,
+      noteImages: [cheatImg],
+      length: 0,
       timer: '',
-      currentNote: ds[0],
-      cheat: 0
+      currentNote: cheatImg,
+      cheat: 0,
+      level: 1
     };
   }
 
@@ -56,7 +42,8 @@ export default class FlashCard extends Component {
   
 
   componentDidMount() {
-    this.nextCard();
+    this.changeLevel();
+    // this.nextCard();
   }
 
 
@@ -113,7 +100,105 @@ export default class FlashCard extends Component {
     
   }
 
+
+  changeLevel(){
+    console.log('changeLevel');
+
+/*
+
+{ name: '', image: require('./assets/cheat.png')}
+
+{ name: 'c4', image: require('./assets/c4.png')}, 
+{ name: 'd4', image: require('./assets/d4.png')}, 
+{ name: 'e4', image: require('./assets/e4.png')}, 
+{ name: 'f4', image: require('./assets/f4.png')}, 
+{ name: 'g4', image: require('./assets/g4.png')}, 
+{ name: 'a4', image: require('./assets/a4.png')}, 
+
+{ name: 'b4', image: require('./assets/b4.png')},
+{ name: 'c5', image: require('./assets/c5.png')}, 
+{ name: 'd5', image: require('./assets/d5.png')}, 
+{ name: 'e5', image: require('./assets/e5.png')}, 
+{ name: 'f5', image: require('./assets/f5.png')}, 
+
+
+{ name: 'g5', image: require('./assets/g5.png')}, 
+{ name: 'a5', image: require('./assets/c6.png')}, 
+{ name: 'b5', image: require('./assets/b5.png')}
+{ name: 'c6', image: require('./assets/c6.png')}
+
+{ name: 'd4', image: require('./assets/d3.png')}, 
+{ name: 'b3', image: require('./assets/b3.png')},
+{ name: 'a3', image: require('./assets/a3.png')},
+{ name: 'g3', image: require('./assets/g3.png')},
+*/
+    var notes = [];
+
+    notes.push(cheatImg);
+
+    let currentLevel = this.state.level;
+
+
+    if(currentLevel <=1){
+
+      notes.push({ name: 'c4', image: require('./assets/c4.png')}); 
+      notes.push({ name: 'd4', image: require('./assets/d4.png')}); 
+      notes.push({ name: 'e4', image: require('./assets/e4.png')}); 
+      notes.push({ name: 'f4', image: require('./assets/f4.png')}); 
+      notes.push({ name: 'g4', image: require('./assets/g4.png')}); 
+      notes.push({ name: 'a4', image: require('./assets/a4.png')});
+      // notes.concat([]);
+    }
+
+    if( (currentLevel > 1) && (currentLevel <=2)){
+        notes.push({ name: 'b4', image: require('./assets/b4.png')});
+        notes.push({ name: 'c5', image: require('./assets/c5.png')}); 
+        notes.push({ name: 'd5', image: require('./assets/d5.png')}); 
+        notes.push({ name: 'e5', image: require('./assets/e5.png')}); 
+        notes.push({ name: 'f5', image: require('./assets/f5.png')});    
+      }
+
+    if((currentLevel > 2) && (currentLevel <=3)){
+        notes.push({ name: 'g5', image: require('./assets/g5.png')});
+        notes.push({ name: 'a5', image: require('./assets/c6.png')});
+        notes.push({ name: 'b5', image: require('./assets/b5.png')});
+        notes.push({ name: 'c6', image: require('./assets/c6.png')});
+    }
+
+    if((currentLevel > 2) && (currentLevel <=4)){
+        notes.push({ name: 'd4', image: require('./assets/d4.png')});
+        notes.push({ name: 'b3', image: require('./assets/b3.png')});
+        notes.push({ name: 'a3', image: require('./assets/a3.png')});
+        notes.push({ name: 'g3', image: require('./assets/g3.png')});
+    }
+
+
+    console.log(notes);
+
+    this.setState({
+      noteImages: notes,
+      length: notes.length
+    }, function(){
+      this.nextCard();
+    });
+
+
+  }
   
+
+  // <View>
+  //       <Picker>
+  //                 <Picker.Item label="Level 0" value="0" />
+  //                 <Picker.Item label="Level 1" value="1" />
+  //               </Picker>
+  //               </View>
+
+
+                  // selectedValue={this.state.level}
+                  // onValueChange={(lang) => {
+                  //   this.changeLevel.bind(this);
+                  // }}
+
   render() {
     return (
       <View style = {styles.container}>
@@ -124,6 +209,18 @@ export default class FlashCard extends Component {
           <Image style={styles.cardImage} source={this.state.currentNote.image} />
           </TouchableHighlight>
           <Text style={[styles.info, {'opacity': (this.state.cheat)?1:0}]}>{this.state.currentNote.name}</Text>
+        </View>
+        <View style={styles.row}>
+        <Slider style={styles.slider}
+          minimumValue={1}
+          maximumValue={4}
+          step={1}
+          onValueChange={(value) => {
+            this.setState({level: value});
+            this.changeLevel.bind(this);
+          }
+        } />
+          <Text>Level {this.state.level} len: {this.state.length}</Text>
         </View>
         <View style={styles.statusbar}>
               <View style={styles.spacer} />
@@ -149,6 +246,12 @@ const styles = StyleSheet.create({
     cardImage:{
       width: 220,
       height: 298,
+    },
+
+    slider: {
+      height: 10,
+      margin: 10,
+      width: 250,
     },
 
     info:{
--- src/menu.js
+++ src/menu.js
@@ -13,7 +13,7 @@ import {
 
 
 var appVer = "v0.1";
-var buildVer = "2017.03.14#7";
+var buildVer = "2017.03.14#8";
 
 // ♬♪♫🎶🎵🎼
 
